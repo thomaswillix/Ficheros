c2270e07ebda6939730a51b6c531463aa8f4012e
Thomas <117021963+thomaswillix@users.noreply.github.com>
2023-11-10 12:43:10 +0100

    Update Manejador.java

--- src/ficherosXML/Ejercicio1/Manejador.java
+++ src/ficherosXML/Ejercicio1/Manejador.java
@@ -2,9 +2,64 @@ package ficherosXML.Ejercicio1;
 
 import java.util.ArrayList;
 
-import org.w3c.dom.Element;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-public class Manejador {
-	private ArrayList<Element> el =  new ArrayList<Element>();
+
+/**
+ *
+ * @author Thomas Freitas
+ */
+public class Manejador extends DefaultHandler {
+	private ArrayList<Alumno> lista = new ArrayList<Alumno>();
 	private Alumno a;
+	private StringBuilder sb = new StringBuilder();
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		super.characters(ch, start, length);
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		switch (qName) {
+		case "Nombre":
+			a.setNombre(sb.toString());
+			break;
+		case "nota1":
+			a.setNota1(Integer.parseInt(sb.toString()));
+			break;
+		case "nota2":
+			a.setNota2(Integer.parseInt(sb.toString()));
+			break;
+		case "proyecto":
+			a.setProyecto(Integer.parseInt(sb.toString()));
+			break;
+		case "practica":
+			a.setPractica(Integer.parseInt(sb.toString()));
+			break;
+		}
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		switch (qName) {
+		case "Alumno":
+			a = new Alumno();
+			lista.add(a);
+			a.setId(attributes.getValue("id"));
+			break;
+		case "nombre":
+		case "nota1":
+		case "nota2":
+		case "proyecto":
+		case "practica":
+			sb.delete(0, sb.length());
+			break;
+		}
+	}
+	 public ArrayList<Alumno> getLista(){
+	        return lista;
+	    }
 }

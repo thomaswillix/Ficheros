28c34b2a11faaeb1a4ef84dbb6dc913e6f1eaa7b
thomaswillix <thomaswfds@gmail.com>
2024-06-08 18:38:27 +0200

    Correccion XML Exercises

--- src/ficherosXML/facturas/Main.java
+++ src/ficherosXML/facturas/Main.java
@@ -2,6 +2,7 @@ package ficherosXML.facturas;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
@@ -23,14 +24,16 @@ import org.xml.sax.SAXException;
  * @author Thomas Freitas
  */
 public class Main {
+
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerConfigurationException, TransformerException {
-        File f =  new File("datos.xml");
-        SAXParserFactory spf=SAXParserFactory.newInstance();
-        SAXParser sp=spf.newSAXParser();
+        File f = new File("files/datos.xml");
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        SAXParser sp = spf.newSAXParser();
         Manejador m = new Manejador();
         sp.parse(f, m);
         Datos d = m.getD();
-
+        ArrayList<Cliente> listaC = d.getClientes();
+        ArrayList<Producto> listaP = d.getProductos();
         System.out.println(d.toString());
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -38,53 +41,51 @@ public class Main {
         Document doc = db.newDocument();
         Element root = doc.createElement("Facturas");
         doc.appendChild(root);
-        for (Cliente c : d.getClientes()) {
-            String prod = c.getArticulo(), desc = "SIN INFORMACION", pr = "SIN INFORMACION", uds = String.valueOf(c.getUnidades());
-            int udsStock = 0;
-            for (Producto p : d.getProductos()) {
-                if (p.getId().equals(c.getArticulo())) {
-                    desc =  p.getId();
-                    pr = String.valueOf(p.getPrecio());
-                    udsStock = p.getStock();
+        for (Cliente c : listaC) {
+            int j = 0;
+            while (j < listaP.size() && !listaP.get(j).getId().equals(c.getArticulo())) {
+                j++;
+            }
+            if (j == listaP.size()) {
+                System.err.println("NO EXISTE EL ARTICULO");
+            } else {
+                if (c.getUnidades()<=listaP.get(j).getStock()) {
+                    Element factura = doc.createElement("Factura");
+                    root.appendChild(factura);
+                    Attr nif = doc.createAttribute("NIF");
+                    nif.appendChild(doc.createTextNode(c.getNif()));
+                    factura.setAttributeNode(nif);
+
+                    Element nombre = doc.createElement("Nombre");
+                    nombre.appendChild(doc.createTextNode(c.getNombre()));
+                    factura.appendChild(nombre);
+
+                    Element descripcion = doc.createElement("Descripcion");
+                    descripcion.appendChild(doc.createTextNode(listaP.get(j).getDescripcion()));
+                    factura.appendChild(descripcion);
+
+                    Element unidades = doc.createElement("Unidades");
+                    unidades.appendChild(doc.createTextNode(String.valueOf(c.getUnidades())));
+                    factura.appendChild(unidades);
+                    listaP.get(j).setStock(listaP.get(j).getStock()-c.getUnidades());
+
+                    Element precio = doc.createElement("Precio");
+                    precio.appendChild(doc.createTextNode(String.valueOf(listaP.get(j).getPrecio())));
+                    factura.appendChild(precio);
+
+                    Element total = doc.createElement("Total");
+                    total.appendChild(doc.createTextNode(String.valueOf(listaP.get(j).getPrecio() * c.getUnidades())));
+                    factura.appendChild(total);
+                } else {
+                    System.err.println("NO SE HA PODIDO COMPLETAR EL PEDIDO DEL CIENTE  " + c.getNombre() + " PORQUE NO HABÍAN SUFICIENTES UNIDADES EN STOCK");
                 }
             }
-            if (c.getUnidades()<=udsStock){
-                Element factura = doc.createElement("Factura");
-                root.appendChild(factura);
-                Attr nif = doc.createAttribute("NIF");
-                nif.appendChild(doc.createTextNode(c.getNif()));
-                factura.setAttributeNode(nif);
-
-                Element nombre = doc.createElement("Nombre");
-                nombre.appendChild(doc.createTextNode(c.getNombre()));
-                factura.appendChild(nombre);
-
-                Element descripcion = doc.createElement("Descripcion");
-
-
-                descripcion.appendChild(doc.createTextNode(desc));
-                factura.appendChild(descripcion);
-
-                Element unidades = doc.createElement("Unidades");
-                unidades.appendChild(doc.createTextNode(uds));
-                factura.appendChild(unidades);
-
-                Element precio = doc.createElement("Precio");
-                precio.appendChild(doc.createTextNode(pr));
-                factura.appendChild(precio);
-
-                Element total = doc.createElement("Total");
-                if(!pr.equals("SIN INFORMACION")) {
-                    total.appendChild(doc.createTextNode(String.valueOf(Integer.parseInt(uds)* Integer.parseInt(pr))));
-                } else total.appendChild(doc.createTextNode("SIN INFORMACION"));
-                factura.appendChild(total);
-            }else System.err.println("NO SE HA PODIDO COMPLETAR EL PEDIDO DEL CIENTE  " + c.getNombre()+ " PORQUE NO HABÍAN SUFICIENTES UNIDADES EN STOCK");
         }
 
         TransformerFactory tf = TransformerFactory.newInstance();
         Transformer transformer = tf.newTransformer();
-        DOMSource ds= new DOMSource(doc);
-        StreamResult sr= new StreamResult(new File("facturas.xml"));
+        DOMSource ds = new DOMSource(doc);
+        StreamResult sr = new StreamResult(new File("files/facturas.xml"));
         StreamResult sr1 = new StreamResult(System.out);
         transformer.transform(ds, sr);
         transformer.transform(ds, sr1);
